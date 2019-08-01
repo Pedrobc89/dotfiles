20407ec9cb1d16433e7434d403cb969daeee5807
Pedro Costa <pedrobc89@gmail.com>
2019-08-01 17:53:33 -0300

    Update enviroment_setup.sh
    setup env test

--- applescripts/enviroment_setup.sh
+++ applescripts/enviroment_setup.sh
@@ -1,2 +1,15 @@
 echo "Enviroment setup"
+cd ~/Documents/Development/Zapay/repos/api
 
+docker-compose up
+
+# tmux split-window -h
+# tmux select-pane -t 1
+# doc up
+
+# sleep 30
+# cd ../warden
+# npm start
+# tmux new-window
+# cd ~/Documents/Development/Zapay/repos/api
+# api
