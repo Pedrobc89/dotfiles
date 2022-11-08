94135a21869aaf0a0466975118c12da6065ef296
Pedro Costa <pedro.costa@mcd.us.com>
2022-11-08 19:43:33 -0300

    latest config

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -8,10 +8,10 @@ tm_color_music=colour16
 tm_active_border_color=colour05
 
 # separators
-# tm_separator_left_bold="◀"
-# m_separator_left_thin="❮"
-# m_separator_right_bold="▶"
-# m_separator_right_thin="❯"
+#tm_separator_left_bold="◀"
+m_separator_left_thin="❮"
+m_separator_right_bold="▶"
+m_separator_right_thin="❯"
 
 set -g status-left-length 32
 set -g status-right-length 150
@@ -19,47 +19,27 @@ set -g status-interval 5
 
 # default statusbar colors
 # set-option -g status-bg colour0
-
-# tmux 2.8
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-set-option -g status-attr default
 #tmux 2.9
-# set-option -g status-style  fg=$tm_color_active,bg=default,default
+set-option -g status-style  fg=$tm_color_active,bg=default,default
 
 # default window title colors
-
-# tmux 2.8
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
 # tmux 2.9
-# set-window-option -g window-status-style fg=$tm_color_inactive,bg=default
-
+set-window-option -g window-status-style fg=$tm_color_inactive,bg=default
 set -g window-status-format "#I #W"
 
 # active window title colors
-# tmux 2.8
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg default
 # tmux 2.9
-# set-window-option -g window-status-current-style fg=$tm_color_active,bg=default
-
+set-window-option -g window-status-current-style fg=$tm_color_active,bg=default
 set-window-option -g  window-status-current-format "#[bold]#I #W"
 
 # pane border
-# tmux 2.8
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_active_border_color
 # tmux 2.9
-# set-option -g pane-border-style fg=$tm_color_inactive
-# set-option -g pane-active-border-style fg=$tm_active_border_color
+set-option -g pane-border-style fg=$tm_color_inactive
+set-option -g pane-active-border-style fg=$tm_active_border_color
 
 # message text
-# tmux 2.8
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
 # tmux 2.9
-# set-option -g message-style fg=$tm_color_active,bg=default
+set-option -g message-style fg=$tm_color_active,bg=default
 
 # pane number display
 set-option -g display-panes-active-colour $tm_color_active
@@ -74,7 +54,7 @@ set-window-option -g clock-mode-colour $tm_color_active
 
 tm_date="#[fg=$tm_color_clock] %R %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
-tm_session_name="[#[fg=$tm_color_feature,bold]#S]"
+tm_session_name="[#[fg=$tm_color_feature,bold]#S#[fg=colour255]]"
 
 set -g status-left $tm_session_name' '
 set -g status-right $tm_tunes' '$tm_date' '$tm_host
