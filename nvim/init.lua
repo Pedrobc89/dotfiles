8f90821fb88d41e0e20874f593cf9e997598ec61
Pedro Costa <pedrobc89@gmail.com>
2023-06-26 23:58:13 -0300

    more nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,1 @@
-require('maps')
-require('settings')
-require('plugs')
-require('plug_settings')
-require('lsp_config')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-
-if is_mac then
-  require('macos')
-end
+require("config.lazy")
