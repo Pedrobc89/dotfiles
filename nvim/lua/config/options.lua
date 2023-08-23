54b628d8c8d7a13cf8ffa19428410b3594b2ab5b
Pedro Costa <pedrobc89@gmail.com>
2023-08-23 20:59:18 -0300

    conceallevel = 0 (json files were bugging me)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
--- Disable automatically comment on next line
-vim.opt.colorcolumn = "100"
--- vim.opt.formatoptions:remove("cro")
-vim.cmd [[set formatoptions-=cro]]
+vim.o.conceallevel = 0
