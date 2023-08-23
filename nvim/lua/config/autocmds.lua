55e62d4a63b04eaaa2efaec51162485c0708a8c8
Pedro Costa <pedrobc89@gmail.com>
2023-08-23 11:31:05 -0300

    Disable auto comment next line

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable automatically comment on next line
+vim.cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
