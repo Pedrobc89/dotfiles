8d2f056c567fd9c70a7142a91e339eadbe817c20
Pedro Costa <pedro.costa@ibm.com>
2024-01-24 19:49:50 +0000

    config updates

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+vim.g.autoformat = false
 vim.o.conceallevel = 0
 
 -- Set colorcolumn = 80
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
+
+-- Add 3rdparty/python/configs/  to runtimepath to allow for python linters to work
+vim.opt.runtimepath:append("./3rdparty/python/configs/")
+
+vim.cmd([[
+  let g:python3_host_prog = '/home/pedro/Documents/Development/IBM/repos/techlabs/dist/export/python/virtualenvs/python-repository/3.8.16'
+]])
--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,6 +1,19 @@
+local function path_to_config(arg_name, filename)
+  local python_techlabs_3rdparty = "/home/pedro/Documents/Development/IBM/repos/techlabs/3rdparty/python/configs/"
+  local cfg_path = python_techlabs_3rdparty .. filename
+  if vim.fn.filereadable(cfg_path) == 1 then
+    return { arg_name, filename }
+  else
+    print("Not Found " .. cfg_path)("coverage.toml")("flake8.ini")("mypy.ini")("pylint.toml")("pytest.toml")
+  end
+  return {}
+end
+
+local pants_file = "/home/pedro/Documents/IBM/Development/repos/techlabs/pants.toml"
+
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
     opts = function()
@@ -13,12 +26,26 @@ return {
           nls.builtins.formatting.shfmt,
 
           -- [[ Python ]]
-          nls.builtins.diagnostics.flake8,
-          nls.builtins.diagnostics.mypy,
-          nls.builtins.diagnostics.pylint,
+          nls.builtins.diagnostics.flake8.with({
+            extra_args = path_to_config("--config", "flake8.ini"),
+          }),
+          nls.builtins.diagnostics.mypy.with({
+            extra_args = { "--config-file", "pants.toml" },
+          }),
+          nls.builtins.diagnostics.pylint.with({
+            -- extra_args = {
+            --   "--rcfile",
+            --   "/home/pedro/Documents/Development/IBM/repos/techlabs/3rdparty/python/configs/pylint.toml",
+            -- },
+            extra_args = { "--rcfile", "pants.toml" },
+          }),
 
-          nls.builtins.formatting.isort,
-          nls.builtins.formatting.black,
+          nls.builtins.formatting.isort.with({
+            extra_args = path_to_config("--settings-file", "isort.toml"),
+          }),
+          nls.builtins.formatting.black.with({
+            extra_args = { "--config", "pants.toml" },
+          }),
         },
       }
     end,
