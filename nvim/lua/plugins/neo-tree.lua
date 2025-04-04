7fa193238c87205c61951e4b1e22a17f085a1926
Pedro Costa <pedrobc89@gmail.com>
2025-04-04 12:28:52 -0300

    updates nvim config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -14,7 +14,7 @@ return {
     opts = {
       filesystem = {
         bind_to_cwd = true,
-        follow_current_file = false,
+        follow_current_file = {enabled = false},
         use_libuv_file_watcher = true,
       },
       window = {
