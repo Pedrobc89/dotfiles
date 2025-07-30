aeeaf04823b44cf4334e1db558d78cd493234594
Pedro Costa <pedrobc89@gmail.com>
2025-07-30 12:32:25 -0300

    feat(install): better link_dotfiles

--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# TODO: fix install fuction
 # TODO: find a better way to define brew items to install based on personal/work and OS
-#
-#
-# apt install git curl
 
 set -eou pipefail
 
+DOTFILES="${HOME}/.dotfiles"
+XDG_CONFIG_HOME="${HOME}/.config"
+
 function install_brew() {
   # Check if Homebrew is installed
-  if command -v brew &>/dev/null; then
-    echo "✅ Homebrew is already installed"
+  if command -v brew &>/dev/null; then echo "✅ Homebrew is already installed"
     return 0
   fi
 
@@ -73,8 +71,7 @@ function brew_install_formulae() {
   local items=(
     bat btop curl dust eza fzf gcc gh go ipython k9s kubernetes-cli lazydocker
     lazygit lua luajit mosh neovim node nvm python@3.13 ripgrep rust starship
-    tealdeer telnet tmux tree-sitter vim wget zoxide zsh zsh-completions
-    zsh-syntax-highlighting
+    tealdeer telnet tmux tree-sitter vim wget zoxide zsh
   )
 
   brew_install_items formula ${items[*]}
@@ -85,7 +82,7 @@ function brew_install_casks() {
   echo "🔍 Checking installed casks..."
   local items=(
     betterdisplay choosy discord docker google-chrome karabiner-elements kitty
-    mqtt-explorer openemu postman raycast
+    mqtt-explorer openemu postman raycast ghostty
   )
 
   brew_install_items cask ${items[*]}
@@ -96,135 +93,16 @@ function brew_install_fonts() {
   local items=(
     font-meslo-lg-nerd-font
     font-jetbrains-mono-nerd-font
+    font-meslo-lg-nerd-font
+    font-caskaydia-cove-nerd-font
+    font-comic-shanns-mono-nerd-font
+    font-fira-code-nerd-font
+    font-iosevka-term-nerd-font
+    font-jetbrains-mono-nerd-font
   )
 
   brew_install_items cask ${items[*]}
 }
-
-
-# function install() {
-#   # Add this to install syntax highliting
-#   # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#
-#   echo "Installing dotfiles"
-#   # check wich operating system we are running
-#   if [ "$(uname)" == "Darwin" ]; then
-#     # Do something under Mac OS X platform
-#     local macos=true
-#     echo "Running on MacOS"
-#   elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-#     # Do something under GNU/Linux platform
-#     local linux=true
-#     echo "Running on Linux"
-#   fi
-#
-#   # update/upgrade apt if linux
-#   if [ "$linux" == true ]; then
-#     echo "Updating apt"
-#     sudo apt update
-#     sudo apt upgrade
-#     sudo apt install build-essential
-#   fi
-#
-#   # TODO: make it non interactive
-#   # install Homebrew for linux as a package manager (non interactive)
-#   # explore NONINTERACTIVE=1 option
-#
-#   # Installing Brew as a package manager
-#   # check if brew is installed (macos) or linuxbrew (linux)
-#   if ! command -v brew &>/dev/null; then
-#     brew_install
-#   fi
-#
-#   # install nvim, vim, tmux, zsh, python@3.11, gcc
-#   brew install nvim vim tmux zsh gcc python3 wget btop bat fzf fd ripgrep
-#   brew install k9s gh dust tealdeer ruby rust lazydocker lazygit tree-sitter lua
-#   brew install npm yarn nodejs go nvm kubectl helm
-#   brew install starship
-#
-#   # install apps and casks for macos
-#   if [ "$macos" == true ]; then
-#     brew install iterm2
-#     brew install --cask docker
-#     brew install --cask google-chrome
-#     brew install --cask 1password
-#     brew install karabiner-elements
-#   elif [[ "$linux" == true ]]; then
-#     brew install xsel docker docker-compose
-#   fi
-#
-#   # install hackfont on macos we can use brew
-#   if [ "$macos" == true ]; then
-#     brew tap homebrew/cask-fonts
-#     brew install --cask font-hack-nerd-font
-#   elif [ "$linux" == true ]; then
-#     # Download the zip file from
-#     curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip --output ~/font-hack-nerd-font.zip
-#     unzip ~/font-hack-nerd-font.zip -d ~/font-hack-nerd-font
-#     if [ ! -d ~/.fonts ]; then
-#       mkdir ~/.fonts
-#     fi
-#     cp ~/font-hack-nerd-font/*.ttf ~/.fonts/
-#     rm -r ~/font-hack-nerd-font ~/font-hack-nerd-font.zip
-#   fi
-#
-#   if [ "$linux" == true ]; then
-#     # Cattpuccin as terminal colorscheme
-#     curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.2.0/install.py | python3 -
-#   elif [ "$macos" == true ]; then
-#
-#   fi
-#
-#   # make zsh as default shell
-#   chsh -s $(which zsh)
-#
-#   # if $SHELL is not equal to $(which zsh) log an error
-#   if [ "$SHELL" != "$(which zsh)" ]; then
-#     echo "Error: zsh is not the default shell"
-#   else
-#     echo "zsh is the default shell"
-#   fi
-#
-#   # TODO: need to spinup neovim to download plugins or ditch LazyVim and use my own
-#   # Download and install LazyVim
-#   [ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim.bak
-#   git clone https://github.com/LazyVim/starter ~/.config/nvim
-#   rm -rf ~/.config/nvim/.git
-#
-#   # Clone TPM (Tmux Plugin Manager)
-#   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-#
-#   # Download and install personal dotfiles
-#   git clone https://github.com/PedroBC89/dotfiles.git ~/.dotfiles/
-#
-#
-#   # create development folder
-#   mkdir -p ~/Documents/Development/
-#
-#   # configure git
-#   git config --global user.name "PedroBC89"
-#   git config --global user.email "pedrobc89@gmail.com"
-#
-#   # add configuration to ssh config file
-#   [ ! -d ~/.ssh ] && mkdir ~/.ssh
-#   [ ! -f ~/.ssh/config ] && touch ~/.ssh/config
-#   echo "Host github.com" >>~/.ssh/config
-#   echo "  HostName github.com" >>~/.ssh/config
-#   echo "  User git" >>~/.ssh/config
-#   echo "  IdentityFile ~/.ssh/github-Pedrobc89" >>~/.ssh/config
-#
-#   # install oh-my-zsh
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#
-#   # Create symlinks to home folder
-#   echo "Installing dotfiles"
-#   ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-#   ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
-#   ln -sf ~/.dotfiles/nvim ~/.config/nvim
-#   if [ "$macos" == true ]; then
-#     ln -sf ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-#   fi
-# }
 
 function install_omz() {
   # install oh-my-zsh
@@ -233,18 +111,23 @@ function install_omz() {
 
 function link_dotfiles() {
 
+
   # Remove previous dotfiles or symlinks
   echo "Removing existing dotfiles"
   # test if file exists before removing
-  [ -f ~/.vimrc ] && rm ~/.vimrc
   [ -f ~/.zshrc ] && rm ~/.zshrc
   [ -f ~/.tmux.conf ] && rm ~/.tmux.conf
-  [ -d ~/.config/nvim ] && rm -r ~/.config/nvim
 
   echo "Linking new dotfiles"
    ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
    ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
-   mkdir -p ~/.config/nvim && ln -sf ~/.dotfiles/nvim ~/.config/nvim
+
+  echo "Replacing config dirs"
+  # for every directory or file inside .dotfiles/config
+  for item in "${DOTFILES}/config/"*; do
+    local item_name=$(basename "$item")
+    ln -sf "$item" "${XDG_CONFIG_HOME}/${item_name}"
+  done
 }
 
 function macos_install() {
